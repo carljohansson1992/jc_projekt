ca109b86f2e2dcc6e78d69b1ead5ea27e18f8daa
Carl Johansson <carljohansson.1992@gmail.com>
2022-03-09 11:03:24 +0100

    Added new model/controller/migartion for recipe

---
+++ database/migrations/2022_03_09_093109_create_recipes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('user_id');
+            $table->text('recipe_name');
+            $table->text('recipe_desc');
+            $table->integer('category_id');
+            $table->text('ingredients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
